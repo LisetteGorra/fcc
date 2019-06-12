b857d36e08c0d8647e0e12eec2d4d33ddaa78b5f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-12 19:10:32 -0400

    Used nested forloop to access each subarray, if statement to set condition

--- largestof4.js
+++ largestof4.js
@@ -1,16 +1,20 @@
 function largestOfFour(arr) {
   var largestNumber = [];
   for (var i = 0; i < arr.length; i++){
-      var tempMax = arr[i][0];
+    var results = arr[i][0];
       for(var j = 0; j < arr[i].length; j++){
-        if( arr[i][j] >= largestNumber){
-            largestNumber = arr[i][j];
+        if( arr[i][j] > results){
+            results = arr[i][j];
         }
       }
-    largestNumber[i] = largestNumber;
+   
     
-  return largestNumber; 
+         largestNumber[i] = results; 
   }  
+   return largestNumber;
 }
+//function largestOfFour(arr) {
+//return arr.map(Function.apply.bind(Math.max, null));
+// } // advanced solution
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
