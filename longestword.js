16d8b34d736d0ae026a837470f34ca16a68ac9b6
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-12 16:11:02 -0400

    Used forloop, if statement, return statemnt to complete function

--- longestword.js
+++ longestword.js
@@ -1,10 +1,12 @@
 function findLongestWordLength(str) {
-    var strSplit = str.split(" ");
-    return strSplit;
-    
-
-
-
+    var strSplit = str.split(" "); 
+    var longestWord = 0; // you need a place to store the length by number 
+    for (var i = 0; i < strSplit.length; i ++){ //looping through the length of the loop, 9 indexes in array, 9 loops
+        if( strSplit[i].length >= longestWord){ //comparing the length of each word to the longest word, looping 9 times between variable and a var[i].length
+            longestWord = strSplit[i].length; //storing new longest length into varaiable longestWord
+        }// length is returning an interger, we store it as a number and not a string for the challenge. 
+    }
+    return longestWord; // this will give you the  new number variable (remember we started at 0 on line 3)
 
 //   var newArray = str.split(" ");
 //   var lngstWord = "";
