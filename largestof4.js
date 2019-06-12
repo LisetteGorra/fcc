42ce151301d152da78592a8ba2b775de8de7ed9c
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-12 17:17:10 -0400

    This program is only giving first 2 subarrays largest number

---
+++ largestof4.js
@@ -0,0 +1,16 @@
+function largestOfFour(arr) {
+  var largestNumber = [];
+  for (var i = 0; i < arr.length; i++){
+      var tempMax = arr[i][0];
+      for(var j = 0; j < arr[i].length; j++){
+        if( arr[i][j] >= largestNumber){
+            largestNumber = arr[i][j];
+        }
+      }
+    largestNumber[i] = largestNumber;
+    
+  return largestNumber; 
+  }  
+}
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
