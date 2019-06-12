357f4d724bd9c4867898c58e17ca62118b707e9f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-12 16:00:13 -0400

    Creating function to find the largest word, used .split() method

--- longestword.js
+++ longestword.js
@@ -1,7 +1,23 @@
 function findLongestWordLength(str) {
-  var newArray = str.split(" ");
-  var sortedArray = newArray.sort((a,b) =>{ return a.length < b.length});
-  return sortedArray[0].length;
+    var strSplit = str.split(" ");
+    return strSplit;
+    
+
+
+
+
+//   var newArray = str.split(" ");
+//   var lngstWord = "";
+//   var lngstWordInt = 0;
+// for (var i = 0; i < newArray.length; i++){
+//     if( newArray[i].length >= lngstWordInt){
+//         lngstWordInt = newArray[i].length;
+//         lngstWord = newArray[i];
+       
+//     }
+
+// }
+//  console.log(lngstWord[i]);
 }
 
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
