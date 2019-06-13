72b79047e5492e889273e6d3ffb639c5831eabe5
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-13 11:35:12 -0400

    Created for loop function that will repeat a string, and return an empty string when num is neg

--- repeatstring.js
+++ repeatstring.js
@@ -1,10 +1,27 @@
-function repeatStringNumTimes(str, num) {
-  var repeatNum = '';
-  while ( num > 0){
-      repeatNum += str;
-      num--;
-  }
-  return repeatNum;
+function repeatString( string, repeatNumTimes){
+    if( repeatNumTimes < 0){
+        return "";
+    }
+    for (var i = 0; i < repeatNumTimes; i++){
+        console.log(string);
+    }
 }
 
-console.log(repeatStringNumTimes("abc", 5));
+repeatString("Arnell", 2);
+
+
+
+
+
+
+
+// function repeatStringNumTimes(str, num) {
+//   var repeatNum = '';
+//   while ( num > 0){
+//       repeatNum += str;
+//       num--;
+//   }
+//   return repeatNum;
+// }
+
+// console.log(repeatStringNumTimes("abc", 5));
