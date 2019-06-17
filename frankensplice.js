fd13737293470e8999aca3001a8c0a2766dd622b
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-17 14:29:30 -0400

    Function will create new array in order, used splice and slice, and forloop

--- frankensplice.js
+++ frankensplice.js
@@ -1,8 +1,8 @@
 function frankenSplice(arr1, arr2, n) {
 var newArray = arr2.slice();
 for (var i = 0 ; i < arr1.length; i++) {
-    newArray.splice(n, 0, arr1[i]);
-    n++;
+    newArray.splice(i, 0, arr1[i]);
+   
 }
 
   return newArray;
