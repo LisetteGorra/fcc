925fd613426b366a1f441c92e01af6a73836e3b2
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-25 19:19:06 -0400

    Example of how to access a property inside nested objects with space in value/string

---
+++ accessingnestedprops.js
@@ -0,0 +1,14 @@
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Glove box can only be accessed like this because of the space in its string, so it requires special characters
