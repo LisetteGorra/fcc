9244b0ae9ffbd9d625486efaed7a0b8804fa789e
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-06-25 19:29:03 -0400

    Added another object inside of array, and accesed both with index[]

---
+++ complexobject.js
@@ -0,0 +1,27 @@
+// Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+    {
+    "artist": "Lovely Jones",
+    "title": "Breaking Silence",
+    "release_year": 2019,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  
+];
